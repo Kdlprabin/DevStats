97bd5aa19102cd4f9061c4ea84b4b1905291de88
prabin <kdlprabin444@gmail.com>
2022-09-02 08:26:09 +0545

    new beginning

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    $('#name').text(profile.getName());
-    $('#email').text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display","block");
-    $(".g-signin2").css("display","none");
-    console.log(profile)
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-}
+
