c59d38f659c09b147418153f18121f3acb6f4a21
prabin <kdlprabin444@gmail.com>
2022-09-02 08:07:40 +0545

    sign in with google

---
+++ script.js
@@ -0,0 +1,11 @@
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    $('#name').text(profile.getName());
+    $('#email').text(profile.getEmail());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display","block");
+    $(".g-signin2").css("display","none");
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+}
