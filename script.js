62bc04cc2bae745e0455eb354c7d05196117b24f
prabin <kdlprabin444@gmail.com>
2022-09-02 10:16:34 +0545

    login and signup page added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function registerBtn(){
+    document.getElementById("signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+}
+function backToLogin(){
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("login").style.display = "block";
+}
